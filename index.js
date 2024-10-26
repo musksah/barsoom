00b59001b3d3c10cc0288f5e977a3858a3a61525
Sebastián Andrés Huérfano Rodríguez <xsebastianhrodriguezx@gmail.com>
2024-10-26 01:31:55 +0000

    initial version

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import * as core from '@actions/core';
+import * as core from '@actions/github';
 
-const nameToGreet = core.getInput('who-to-greet');
-const output_value = `Hello ${nameToGreet}!`;
-core.setOutput('greeting', output_value);
+try{
+    const nameToGreet = core.getInput('who-to-greet');
+    const output_value = `Hello ${nameToGreet}!`;
+    core.setOutput('greeting', output_value);
+} catch (error) {
+    core.setFailed(error.message);
+}
